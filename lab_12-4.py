0c78f2aa1fd2b15e6c005b5d43c01ecceeb925dd
p22yxi <p22yxi@fairfieldprep.org>
2022-02-16 10:20:04 -0500

    4

--- lab_12-4.py
+++ lab_12-4.py
@@ -1,4 +1,6 @@
 # Yongdong Xi
-days_of_of_the_week = ('1', '2', '3', '4', '5', '6', '7')
+days_of_of_the_week = ('0', '1', '2', '3', '4', '5', '6')
+# Create a tuple
 (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) = days_of_of_the_week
+# variable that represents each day using a single statement.
 print(Tuesday)
