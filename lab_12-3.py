40f37c1d22b19c1bce547ed2fd66d2b297582a9d
p22yxi <p22yxi@fairfieldprep.org>
2022-02-16 10:08:41 -0500

    3,4

---
+++ lab_12-3.py
@@ -0,0 +1,5 @@
+# Yongdong Xi
+weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+weekends = ('Saturday', 'Sunday')
+days_of_of_the_week = weekdays + weekends
+print(days_of_of_the_week)
---
+++ lab_12-4.py
@@ -0,0 +1,4 @@
+# Yongdong Xi
+days_of_of_the_week = ('1', '2', '3', '4', '5', '6', '7')
+(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) = days_of_of_the_week
+print(Tuesday)
