f69be33dac52c08698739938e698f9b81e0b73bc
p22yxi <p22yxi@fairfieldprep.org>
2022-02-23 09:31:14 -0500

    5

---
+++ lab_12-5.py
@@ -0,0 +1,14 @@
+# Yongdong Xi
+class_row = {'B202_01': 'none', 'B202_02': 'Mateo Bonilla', 'B202_03': 'Diao Jason', 'B202_04': 'Hart Jordan', 'B202_05': 'Elsheikh Mohamed', 
+'B202_06': 'Giroux Michael', 'B202_07': 'Gussen Charlie', 'B202_08': 'Healy Declan', 
+'B202_09': 'none', 'B202_10': 'none', 'B202_11': ' Iacono Jack', 'B202_12': 'Iaropoli James', 
+'B202_13': 'Lugo Ryan', 'B202_14': 'Mendizabal Luis', 'B202_15': 'Rogers Sean', 'B202_16': 'Nishiyama Aiden', 
+'B202_17': 'Nolan Ian', 'B202_18': 'O\'Brien Colin', 'B202_19': 'Pearson Timmy', 'B202_20': 'Petrelli Cam', 
+'B202_21': 'none', 'B202_22': 'none', 'B202_23': 'Piselli Nolan', 'B202_24': 'Stewart Ryan', 'B202_25': 'Sullivan Chris', 
+'B202_26': 'Trautz Spencer', 'B202_27': 'none', 'B202_28': 'Yongdong Xi', 'B202_29': 'none', 'B202_30': 'none'}
+
+def new_guy():
+    student = input("Who is sitting in the B202_28 right now")
+    class_row['B202_28'] = student
+
+print(class_row)
